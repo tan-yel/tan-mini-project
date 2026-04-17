b93ef7a9ca55345ac6ff7b82c83a739db251add6
tan-yel <tanyelkemal@gmail.com>
2026-04-17 13:48:48 +0100

    finished game

--- noughts-and-crosses/app.js
+++ noughts-and-crosses/app.js
@@ -1,5 +1,7 @@
-const gameBoard = document.querySelector("#gameboard")
-const infoDisplay = document.querySelector("#info")
+const gameBoard = document.getElementById("gameboard")
+const infoDisplay = document.getElementById("info")
+const pokeName = document.getElementById("pokeName")
+const sprite = document.getElementById("sprite")
 const cells = ["", "", "", "", "", "", "", "", ""]
 const players = ["Player 1", "Player 2"]
 let turn = players[0]
@@ -8,10 +10,10 @@ let turn = players[0]
 infoDisplay.textContent = `${players[0]} goes first`
 
 function createBoard() {
-    cells.forEach((cells, index) => {
+    cells.forEach((cell) => {
         const squareElement = document.createElement("div")
         squareElement.classList.add("square")
-        squareElement.id = index
+        squareElement.id = cell
         squareElement.addEventListener("click", addTurn)
         gameBoard.append(squareElement)
     })
@@ -23,9 +25,9 @@ function addTurn(e) {
     console.log("clicked", e)
     const turnDisplay = document.createElement("div")
     if (turn == players[0]){
-        turnDisplay.classList.add('circle')
-    } else {
         turnDisplay.classList.add('cross')
+    } else {
+        turnDisplay.classList.add('circle')
     }
     e.target.append(turnDisplay)
     turn = turn === players[0] ? players[1] : players[0] // if current turn is circle assign cross else assign circle
@@ -44,8 +46,8 @@ function checkScore () {
     winningCombos.forEach(array => {
         const circleWins = array.every(cell => 
             squares[cell].firstElementChild?.classList.contains("circle"))
-        if (circleWins) {
-            infoDisplay.textContent = `${turn} Wins!`
+            if (circleWins) {
+                infoDisplay.textContent = `${turn} Wins!`
             squares.forEach(square => square.removeEventListener("click", addTurn))
         }
     })
@@ -58,4 +60,21 @@ function checkScore () {
             squares.forEach(square => square.removeEventListener("click", addTurn))
         }
     })
+}
+
+// Pokemon bit
+async function generateRandomPokemon() {
+    const pokemonId = Math.floor(Math.random() * 500) + 1
+    const url = `https://pokeapi.co/api/v2/pokemon/${pokemonId}`
+    try {
+        const response = await fetch(url)
+        if (!response.ok) throw new Error('Not found')
+            
+        const data = await response.json()
+        pokeName.textContent = data.name.toUpperCase()
+        sprite.src = data.sprites.front_default
+    }
+    catch(error){
+        return { error: 'Not found' }
+    }
 }
