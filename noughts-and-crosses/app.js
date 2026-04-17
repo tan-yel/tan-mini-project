2393d1e48bb21d9b155892298d9e3f59cfa61bde
tan-yel <tanyelkemal@gmail.com>
2026-04-17 09:20:26 +0100

    working logic

--- noughts-and-crosses/app.js
+++ noughts-and-crosses/app.js
@@ -1,9 +1,11 @@
 const gameBoard = document.querySelector("#gameboard")
 const infoDisplay = document.querySelector("#info")
 const cells = ["", "", "", "", "", "", "", "", ""]
-let turn = "circle"
+const players = ["Player 1", "Player 2"]
+let turn = players[0]
 
-infoDisplay.textContent = "Circle goes first"
+
+infoDisplay.textContent = `${players[0]} goes first`
 
 function createBoard() {
     cells.forEach((cells, index) => {
@@ -20,10 +22,14 @@ createBoard()
 function addTurn(e) {
     console.log("clicked", e)
     const turnDisplay = document.createElement("div")
-    turnDisplay.classList.add(turn)
+    if (turn == players[0]){
+        turnDisplay.classList.add('circle')
+    } else {
+        turnDisplay.classList.add('cross')
+    }
     e.target.append(turnDisplay)
-    turn = turn === "circle" ? "cross" : "circle" // if current turn is circle assign cross else assign circle
-    infoDisplay.textContent = "it is now " + turn + "'s turn"
+    turn = turn === players[0] ? players[1] : players[0] // if current turn is circle assign cross else assign circle
+    infoDisplay.textContent = "Now, " + turn + " it is your turn!"
     e.target.removeEventListener("click", addTurn)
     checkScore()
 }
@@ -39,8 +45,8 @@ function checkScore () {
         const circleWins = array.every(cell => 
             squares[cell].firstElementChild?.classList.contains("circle"))
         if (circleWins) {
-            infoDisplay.textContent = "Circle Wins!"
-            squares.forEach(square => square.replaceWith(square.cloneNode(true)))
+            infoDisplay.textContent = `${turn} Wins!`
+            squares.forEach(square => square.removeEventListener("click", addTurn))
         }
     })
 
@@ -48,9 +54,8 @@ function checkScore () {
         const crossWins = array.every(cell => 
             squares[cell].firstElementChild?.classList.contains("cross"))
         if (crossWins) {
-            infoDisplay.textContent = "Cross Wins!"
-            squares.forEach(square => square.replaceWith(square.cloneNode(true)))
+            infoDisplay.textContent = `${turn} Wins!`
+            squares.forEach(square => square.removeEventListener("click", addTurn))
         }
     })
-
 }
